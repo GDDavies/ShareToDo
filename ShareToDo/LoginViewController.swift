1c4148b8d6527e90f3a2677d46cfa4bfcf322ab2
George <gdaviesdev@gmail.com>
2017-02-04 23:13:48 +0000

    Setup firebase login and signup

--- ShareToDo/LoginViewController.swift
+++ ShareToDo/LoginViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,44 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func didTapLogin(_ sender: UIButton) {
+        guard let email = emailField.text,
+            let password = passwordField.text,
+            email.characters.count > 0,
+            password.characters.count > 0 else {
+                self.showAlert(message: "Enter an email and a password.")
+                return
+        }
+        
+        FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                if error._code == FIRAuthErrorCode.errorCodeUserNotFound.rawValue {
+                    self.showAlert(message: "There are no users with the specified account.")
+                } else if error._code == FIRAuthErrorCode.errorCodeWrongPassword.rawValue {
+                    self.showAlert(message: "Incorrect username or password.")
+                } else {
+                    self.showAlert(message: "Error: \(error.localizedDescription)")
+                }
+                print(error.localizedDescription)
+                return
+            }
+            
+            if let user = user {
+                AuthenticationManager.sharedInstance.didLogIn(user: user)
+                self.performSegue(withIdentifier: "ShowToDosFromLogin", sender: nil)
+            }
+        }
+    }
+    
+    func showAlert(message: String) {
+        // create the alert
+        let alert = UIAlertController(title: "ShareToDo", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
--- ShareToDo/SignUpViewController.swift
+++ ShareToDo/SignUpViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +26,61 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTapSignUp(_ sender: UIButton) {
+        guard let name = nameField.text,
+            let email = emailField.text,
+            let password = passwordField.text,
+            name.characters.count > 0,
+            email.characters.count > 0,
+            password.characters.count > 0
+            else {
+                self.showAlert(message: "Enter a name, an email and a password.")
+                return
+        }
+        
+        FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                if error._code == FIRAuthErrorCode.errorCodeInvalidEmail.rawValue {
+                    self.showAlert(message: "Enter a valid email.")
+                } else if error._code == FIRAuthErrorCode.errorCodeEmailAlreadyInUse.rawValue {
+                    self.showAlert(message: "Email already in use.")
+                } else {
+                    self.showAlert(message: "Error: \(error.localizedDescription)")
+                }
+                print(error.localizedDescription)
+                return
+            }
+            
+            if let user = user {
+                self.setUserName(user: user, name: name)
+            }
+        }
     }
-    */
+
+    func setUserName(user: FIRUser, name: String) {
+        let changeRequest = user.profileChangeRequest()
+        changeRequest.displayName = name
+        
+        changeRequest.commitChanges(){ (error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            AuthenticationManager.sharedInstance.didLogIn(user: user)
+            self.performSegue(withIdentifier: "ShowToDoList", sender: nil)
+        }
+    }
+    
+    func showAlert(message: String) {
+        // create the alert
+        let alert = UIAlertController(title: "ShareToDo", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
