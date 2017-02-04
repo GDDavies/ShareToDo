e011e392d27b602d25a4dfe91022c14735467777
George <gdaviesdev@gmail.com>
2017-02-04 22:52:08 +0000

    Setup

---
+++ ShareToDo/AuthenticationManager.swift
@@ -0,0 +1,25 @@
+//
+//  AuthenticationManager.swift
+//  ShareToDo
+//
+//  Created by George Davies on 04/02/2017.
+//  Copyright © 2017 George Davies. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthenticationManager: NSObject {
+    
+    static let sharedInstance = AuthenticationManager()
+    
+    var loggedIn = false
+    var userName: String?
+    var userId: String?
+    
+    func didLogIn(user: FIRUser) {
+        AuthenticationManager.sharedInstance.userName = user.displayName
+        AuthenticationManager.sharedInstance.loggedIn = true
+        AuthenticationManager.sharedInstance.userId = user.uid
+    }
+}
