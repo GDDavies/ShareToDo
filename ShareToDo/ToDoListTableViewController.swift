910670026ac78421ca389149fe2cb190a4ee2dec
George <gdaviesdev@gmail.com>
2017-02-05 00:44:34 +0000

    Reads firebase

---
+++ ShareToDo/ToDoListTableViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ToDoListTableViewController.swift
+//  ShareToDo
+//
+//  Created by George Davies on 04/02/2017.
+//  Copyright © 2017 George Davies. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ToDoListTableViewController: UITableViewController {
+    
+    var toDoList = [String]()
+    //var toDoList = ["This","That","Other"]
+    var ref: FIRDatabaseReference!
+    private var databaseHandle: FIRDatabaseHandle!
+    var senderId: String?
+    var senderDisplayName: String?
+    
+    @IBAction func signOut(sender: UIBarButtonItem) {
+        let firebaseAuth = FIRAuth.auth()
+        do {
+            try firebaseAuth?.signOut()
+            AuthenticationManager.sharedInstance.loggedIn = false
+            dismiss(animated: true, completion: nil)
+        } catch let signOutError as NSError {
+            print ("Error signing out: \(signOutError)")
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        senderId = AuthenticationManager.sharedInstance.userId
+        senderDisplayName = AuthenticationManager.sharedInstance.userName
+        
+        ref = FIRDatabase.database().reference(fromURL: "https://sharetodo-2bbee.firebaseio.com/")
+        setupData()
+    }
+    
+    func setupData() {
+        databaseHandle = ref.child("todos").observe(.childAdded, with: { (snapshot) -> Void in
+            if let value = snapshot.value as? [String:AnyObject] {
+                //let id = value["senderId"] as! String
+                let text = value["text"] as! String
+                //let name = value["senderDisplayName"] as! String
+                
+                self.toDoList.append(text)
+                print(self.toDoList)
+                self.tableView.reloadData()
+                
+                //self.addToDo(id: id, text: text, name: name)
+                //self.finishReceivingMessage()
+            }
+        })
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDoList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        print("ok... \(toDoList[indexPath.row])")
+
+        // Configure the cell...
+        cell.textLabel?.text = toDoList[indexPath.row]
+
+        return cell
+    }
+    
+    func addToDo(id: String, text: String, name: String) {
+        toDoList.append(text)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        self.ref.removeObserver(withHandle: databaseHandle)
+    }
+    
+    func setData() {
+        let toDoRef = ref.child("todos").childByAutoId()
+        let todo = [
+            "text": "test todo",
+            "senderId": senderId!,
+            "senderDisplayName": senderDisplayName!
+        ]
+        toDoRef.setValue(todo)
+    }
+
+}
